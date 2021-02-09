acc7d54d78e30142900ce4c125e8b3e426d528e4
Aya Zeid <71414201+ayazeid@users.noreply.github.com>
2021-02-09 16:51:56 +0200

    blog version.1 updated

--- app.js
+++ app.js
@@ -233,7 +233,12 @@ List.findOneAndUpdate({name:listName},{$pull: {items: {_id:itemID}}}, function(e
 
 
 
+//port
 
-    app.listen(3000, function() {
-      console.log("Server started on port 3000");
-    });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server has started succefully");
+});
